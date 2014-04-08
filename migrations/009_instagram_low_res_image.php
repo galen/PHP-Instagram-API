6e6cc0489820bf8369a2c4212489a8fce1e2af00
Tristan Bettany <tristan.bettany@propcom.co.uk>
2014-04-08 10:36:15 +0100

    Storing Low resolution image in db for easy and efficiant use

---
+++ migrations/009_instagram_low_res_image.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace Fuel\Migrations;
+
+class Instagram_Tags_Many_To_Many
+{
+	function up()
+	{	
+		try 
+		{
+			\DB::start_transaction();
+
+			\DBUtil::add_fields('instagram__images', array(
+				'lowres_img' => array('type' => 'varchar', 'null' => true),
+			));
+
+			\DB::commit_transaction();
+		} 
+		catch (\Exception $e)
+		{
+			\DB::rollback_transaction();
+			\Cli::error(sprintf('Up Migration Failed - %s - %s', $e->getMessage(), __FILE__));
+			return false;
+		}
+
+		\Cli::write('Migrated Up Successfully: ' . __FILE__, 'green');
+	}
+
+	function down()
+	{
+		try
+		{
+			\DB::start_transaction();
+
+			\DBUtil::drop_fields('instagram__images', array(
+				'lowres_img'
+			));
+
+			\DB::commit_transaction();
+		}
+		catch (\Exception $e)
+		{
+			\DB::rollback_transaction();
+			\Cli::error(sprintf('Up Migration Failed - %s - %s', $e->getMessage(), __FILE__));
+			return false;
+		}
+
+		\Cli::write('Migrated Down Successfully: ' . __FILE__, 'green');		
+	}
+
+}
