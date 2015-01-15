85fe1ddb10613871dece18d0c9bfc7640efb2300
James Kirby <james.kirby@propcom.co.uk>
2015-01-15 15:07:41 +0000

    Adding a config option to define where the menu is to be added to.

--- config/instagram.php
+++ config/instagram.php
@@ -7,5 +7,11 @@ return array(
 		'redirect_uri'  => '',
 		'scope'         => array('basic'),
 		'display'       => ''
-	)
+	),
+
+	/**
+	 * Change the place for the defaul nav to be placed.
+	 */
+	'nav' => 'Your CMS'
+
 );
