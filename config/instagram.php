8c03fc0e1b59d0f09439b84eeda98e29e6a3a946
Robert O'Brien <robert.obrien@propcom.co.uk>
2016-06-06 14:04:55 +0100

    [Config] Add default config to use Propeller central client

--- config/instagram.php
+++ config/instagram.php
@@ -2,10 +2,10 @@
 
 return array(
 	'auth' => array(
-		'client_id'     => '',
-		'client_secret' => '',
-		'redirect_uri'  => '',
-		'scope'         => array('basic'),
+		'client_id'     => '8d316ba637af488b91ad01e2b2ac41ea',
+		'client_secret' => '97ba5bfd2d2743aab12f5e16f3368fd1',
+		'redirect_uri'  => \Uri::base()."admin/instagram/handler/subscribe",
+		'scope'         => array('basic', 'public_content'),
 		'display'       => ''
 	),
 
